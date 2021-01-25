b5eae887494207d7a380ca41744ac269e92bfeb8
Elisia <elisialight@gmail.com>
2021-01-25 04:42:32 +0300

    реализован клик на кнопку добавления в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -10,3 +10,6 @@ class LoginPageLocators():
     SIGN_UP_email_field = (By.CSS_SELECTOR, "#id_registration-email")
     SIGN_UP_PASSWD_field = (By.CSS_SELECTOR, "#id_registration-password1")
     SIGN_UP_repiet_passwd_field = (By.CSS_SELECTOR, "#id_registration-password2")
+    
+class ProductLocators():
+    CART_button = (By.CSS_SELECTOR, ".btn-add-to-basket")
